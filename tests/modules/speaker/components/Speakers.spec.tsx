126a2136bf97e477497217c429d0a99be7cf6900
Asier Marqués <asiermarques@gmail.com>
2023-04-27 17:36:00 +0200

    speaker test clean imports

--- tests/modules/speaker/components/Speakers.spec.tsx
+++ tests/modules/speaker/components/Speakers.spec.tsx
@@ -1,12 +1,7 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import AgendaContainer from "../../../../src/modules/agenda/components/AgendaContainer";
-import AgendaRepository from "../../../../src/modules/agenda/domain/AgendaRepository";
-import { agendaBasicStructureWithTalkStub } from "../../../__stubs__/agenda";
-import { TrackContentType } from "../../../../src/modules/agenda/domain/TrackContentType";
 import { speakerStub } from "../../../__stubs__/speaker";
-import Speaker from "../../../../src/modules/speaker/templates/speaker";
 import Speakers from "../../../../src/modules/speaker/components/Speakers";
 
 describe("Speakers Component", () => {
